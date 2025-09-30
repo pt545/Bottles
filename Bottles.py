9eeadb1c6e97e497f82d8b0421cc65da09f0e38e
pt545 <pt545@exeter.ac.uk>
2025-09-30 12:50:49 +0100

    Add files via upload

---
+++ Bottles.py
@@ -0,0 +1,11 @@
+bottles = 10
+
+while bottles > 0:
+    print(bottles, "green bottles hanging on the wall,")
+    print(bottles, "green bottles hanging on the wall,")
+    print("and if one green bottle should accidentally fall,")
+    print("they'll be", bottles-1, "green bottles hanging on the wall.")
+    print()
+    bottles = bottles - 1
+
+print('All gone')

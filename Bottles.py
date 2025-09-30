67326d27ee08f919d1dc55048238f22b3e3641a4
Tap96 <arrudapedro966@gmail.com>
2025-09-30 12:58:12 +0100

    Update Bottles.py

--- Bottles.py
+++ Bottles.py
@@ -1,11 +1,11 @@
-bottles = 20
+bottles = 0
 
-while bottles > 0:
+while bottles < 10:
     print(bottles, "green bottles hanging on the wall,")
     print(bottles, "green bottles hanging on the wall,")
-    print("and if one green bottle should accidentally fall,")
-    print("they'll be", bottles-1, "green bottles hanging on the wall.")
+    print("and if one green bottle should accidentally (appear?),")
+    print("they'll be", bottles+1, "green bottles hanging on the wall.")
     print()
-    bottles = bottles - 1
+    bottles = bottles + 1
 
-print('All gone')
+#print('All gone')

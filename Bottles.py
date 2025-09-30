e3188fd256e61c545e455e08e130d4cadcf81d90
Tap96 <arrudapedro966@gmail.com>
2025-09-30 12:59:18 +0100

    Update Bottles.py

--- Bottles.py
+++ Bottles.py
@@ -3,9 +3,9 @@ bottles = 0
 while bottles < 10:
     print(bottles, "green bottles hanging on the wall,")
     print(bottles, "green bottles hanging on the wall,")
-    print("and if one green bottle should accidentally (appear?),")
-    print("they'll be", bottles+1, "green bottles hanging on the wall.")
+    print("and if three green bottles should accidentally (appear?),")
+    print("they'll be", bottles+3, "green bottles hanging on the wall.")
     print()
-    bottles = bottles + 1
+    bottles = bottles + 3
 
 #print('All gone')
